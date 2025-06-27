779beeb0e1077ad7e65e5aaf8b7789d37ea5ebcf
Jorge Dominguez <jorge.dominguez@bvgaboo.com>
2025-06-27 10:05:21 -0600

    Add group-anagrams functionality: implement grouping of anagrams in group-anagrams.ts and document the problem in group-anagram.md

---
+++ group-anagrams/group-anagrams.ts
@@ -0,0 +1,28 @@
+// We can sort the strings and then group them together
+// We can use a map to store the sorted string as key and the array of strings as value
+// We can then return the values of the map
+
+// we receive an array of strings as parameter
+function groupAnagrams(strs: string[]): string[][] {
+    // let's use a hash map to store the sorted string as key and the array of strings as value
+    const groupedAnagrams = new Map<string, string[]>();
+
+    // let's iterate through the array of strings to group the anagrams
+    for (const str of strs) {
+        // create a sorted version of the string to use as reference and we are going to sort alphabetically
+        // we are going to split the string into an array of characters first
+        // then we are going to sort the array of characters
+        // then we are going to join the array of characters back into a string
+        const sortedStr = str.split('').sort().join('');
+
+        // Group each array of strings by the sorted string
+        // if the sorted string is not in the map, we create a new array of strings with the original string
+        if (!groupedAnagrams.has(sortedStr)) {
+            groupedAnagrams.set(sortedStr, [])
+        }
+        groupedAnagrams.get(sortedStr)!.push(str);
+    };
+
+    // return the values of the map as an array of arrays
+    return Array.from(groupedAnagrams.values());
+}
