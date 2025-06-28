90820450e62ad76409bd70f0ff3850e1acddb2bd
Jorge Dominguez <jorge.dominguez@bvgaboo.com>
2025-06-28 11:58:11 -0600

    Add isPalindromeTwo function: enhance palindrome checking by allowing replacement of multiple consecutive non-alphanumeric characters in valid-palindrome.ts

--- valid-palindrome/valid-palindrome.ts
+++ valid-palindrome/valid-palindrome.ts
@@ -2,3 +2,9 @@ function isPalindrome(s: string): boolean {
     const sortedString = s.toLowerCase().replace(/[^a-z0-9]/g,'');
     return sortedString === sortedString.split('').reverse().join('');
 };
+
+// the regex pattern was missing the + sign so it can replace multiple consecutive non-alphanumeric characters at once
+function isPalindromeTwo(s: string): boolean {
+    const normalizedString = s.replace(/[^A-Za-z0-9]+/g,'').toLowerCase();
+    return normalizedString === normalizedString.split('').reverse().join('');
+}
